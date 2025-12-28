6689753a454844332a768d1ed149f5693d9a18e4
MilaGrishkova <49581816+MilaGrishkova@users.noreply.github.com>
2025-12-28 17:45:40 +0100

    Create CheckGround.cs

---
+++ 🎮GameDev/Unity/3D Spiel/Code/1Scene/CheckGround.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckGround : MonoBehaviour
+{
+    public float Distance;
+    public LayerMask LayerMask;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = new Ray(transform.position, Vector3.up);
+        Debug.DrawRay(ray.origin, ray.direction * Distance);
+
+        if(Physics.Raycast(ray, Distance, LayerMask))
+        {
+            GetComponent<Renderer>().material.color = Color.red * 0.7f;
+        }
+        else
+        {
+            GetComponent<Renderer>().material.color = Color.grey;
+        }
+    }
+}
